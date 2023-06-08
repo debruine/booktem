db0a06ffab485c16c2ca5d81c38cc85ef0c26fdb
debruine <debruine@gmail.com>
2023-06-08 12:25:30 +0100

    change github actions

--- inst/quarto/R/booktem_setup.R
+++ inst/quarto/R/booktem_setup.R
@@ -1,26 +1,28 @@
 # booktem functions
 # do not edit unless you're sure what you're doing!!!!!
 
-suppressPackageStartupMessages({
-  library(stats) # stops dplyr::filter being masked
-  library(knitr)
-})
+if (requireNamespace("knitr", quietly = TRUE)) {
+  suppressPackageStartupMessages({
+    library(stats) # stops dplyr::filter being masked
+    library(knitr)
+  })
 
-## set class for a chunk using class="className" ----
-knitr::knit_hooks$set(class = function(before, options, envir) {
-  if (before) {
-    sprintf("<div class = '%s'>", options$class)
-  } else {
-    "</div>"
-  }
-})
+  ## set class for a chunk using class="className" ----
+  knitr::knit_hooks$set(class = function(before, options, envir) {
+    if (before) {
+      sprintf("<div class = '%s'>", options$class)
+    } else {
+      "</div>"
+    }
+  })
 
-## verbatim code chunks ----
-knitr::knit_hooks$set(verbatim = function(before, options, envir) {
-  if (before) {
-    sprintf("<div class='verbatim'><pre class='sourceCode r'><code class='sourceCode R'>&#96;&#96;&#96;{%s}</code></pre>", options$verbatim)
-  } else {
-    "<pre class='sourceCode r'><code class='sourceCode R'>&#96;&#96;&#96;</code></pre></div>"
-  }
-})
+  ## verbatim code chunks ----
+  knitr::knit_hooks$set(verbatim = function(before, options, envir) {
+    if (before) {
+      sprintf("<div class='verbatim'><pre class='sourceCode r'><code class='sourceCode R'>&#96;&#96;&#96;{%s}</code></pre>", options$verbatim)
+    } else {
+      "<pre class='sourceCode r'><code class='sourceCode R'>&#96;&#96;&#96;</code></pre></div>"
+    }
+  })
 
+}
--- inst/quarto/R/my_setup.R
+++ inst/quarto/R/my_setup.R
@@ -1,16 +1,19 @@
 # book-specific code to include on every page
 
-library(glossary)
-glossary::glossary_path("include/glossary.yml")
-glossary_popup("click") # "click", "hover" or "none"
+if (requireNamespace("knitr", quietly = TRUE)) {
 
-# default knitr options ----
-knitr::opts_chunk$set(
-  echo       = TRUE,
-  results    = "hold",
-  out.width  = '100%',
-  fig.width  = 8,
-  fig.height = 5,
-  fig.align  = 'center',
-  dpi = 96
-)
+  library(glossary)
+  glossary::glossary_path("include/glossary.yml")
+  glossary_popup("click") # "click", "hover" or "none"
+
+  # default knitr options ----
+  knitr::opts_chunk$set(
+    echo       = TRUE,
+    results    = "hold",
+    out.width  = '100%',
+    fig.width  = 8,
+    fig.height = 5,
+    fig.align  = 'center',
+    dpi = 96
+  )
+}
